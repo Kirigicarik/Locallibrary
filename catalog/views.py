539faa2d2ae6989fde3d935a36c607df6347efd8
Kirigica MINAKU <kirigicaminaku2020@gmail.com>
2020-03-27 23:52:42 +0200

    +

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from django.views import generic 
 
 # Create your views here.
+from catalog.models import * 
+
+def index(request):
+    """ View function for home page of the site """
+    #Generate counts of some of the main objects
+    num_books = Book.objects.filter(title__icontains='i').count()
+    num_instances = BookInstance.objects.all().count()
+    
+    #Available books (status ='a')
+    num_instances_available = BookInstance.objects.filter(status__exact = 'a').count()
+
+    # The 'all()' is implied by default
+    num_authors = Author.objects.count()
+
+    #Generate counts for genre and books that contains a particular word
+    num_genres = Genre.objects.filter(name__icontains='f').count()
+    
+    context = {
+        'num_books':num_books,
+        'num_instances':num_instances,
+        'num_instances_available':num_instances_available,
+        'num_authors':num_authors,
+        'num_genres':num_genres,
+        }
+
+#Render the HTML template index.html with the data in the context variable 
+    return render(request,'index.html',context=context)
+
+from django.views import generic 
+# class_based view 
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 3
+
+class BookDetailView(generic.DetailView):
+    model = Book
+         
+    
+
+
+    
+
